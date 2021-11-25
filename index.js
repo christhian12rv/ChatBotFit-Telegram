197f5d0779451ba9ba6aa612817095ec9b2b4739
Christhian Rezende Vieira <christhian11rv@hotmail.com>
2021-11-25 17:04:24 -0300

    create chatbot.json and dialogflow

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const telegramBot = require("node-telegram-bot-api");
+const TelegramBot = require("node-telegram-bot-api");
 
 const token = '2107101662:AAEjMPfw1hMOIRlfFr7vdlPpG2ZcLA0SHxk';
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+    const chatId = msg.chat.id;
+
+    console.log(msg.text);
+    bot.sendMessage(chatId, 'Obrigado por sua mensagem');
+})
