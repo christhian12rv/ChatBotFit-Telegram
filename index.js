83cb1483c77df5ea21e82683ca5d532638e5cfad
Christhian Rezende Vieira <christhian11rv@hotmail.com>
2021-11-26 16:09:36 -0300

    Response videos to user

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const TelegramBot = require("node-telegram-bot-api");
+const dialogFlow = require("./dialogflow");
+const youtube = require('./youtube');
 
 const token = '2107101662:AAEjMPfw1hMOIRlfFr7vdlPpG2ZcLA0SHxk';
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.on("message", (msg) => {
+bot.on("message", async (msg) => {
     const chatId = msg.chat.id;
-
     console.log(msg.text);
-    bot.sendMessage(chatId, 'Obrigado por sua mensagem');
+
+    const dfResponse = await dialogFlow.sendMessage(chatId.toString(), msg.text);
+
+    let responseText = dfResponse.text;
+    console.log(dfResponse);
+    if (dfResponse.intent == 'Treino Específico') {
+        console.log('isso');
+        responseText = await youtube.searchVideoURL(responseText, dfResponse.fields.corpo.stringValue);
+    }
+    bot.sendMessage(chatId, responseText);
 })
--- youtube.js
+++ youtube.js
@@ -4,9 +4,18 @@ const config = require("./youtube-config");
 const youtube = new Youtube();
 youtube.setKey(config.key);
 
-youtube.search('Exercício em casa para bíceps', 2, (error, result) => {
-    if (!error)
-        console.log(JSON.stringify(result, null, 2));
-    else
-        console.log("Deu erro");
-});
+
+function searchVideoURL(message, queryText) {
+    return new Promise((resolve, reject) => {
+        youtube.search('Exercícios de ' + queryText, 2, (error, result) => {
+            if (!error) {
+                const videoIds = result.items.map((item) => item.id.videoId).filter(item => item);
+                const youtubeLinks = videoIds.map(videoId => 'https://www.youtube.com/watch?v=' + videoId);
+                resolve(message + " " + youtubeLinks.join(', '));
+            } else
+                reject('Deu erro');
+        });
+    })
+}
+
+module.exports.searchVideoURL = searchVideoURL;
